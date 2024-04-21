3688c33848251136f7d158d54efb133460e1607c
darian <xxy.moming@gmail.com>
2024-04-21 13:27:56 +0800

    Initial Commit

---
+++ Meme Creator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Meme Creator
+//
+//  Created by Darian Mitchell  on 2024/4/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Meme Creator/Meme_CreatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Meme_CreatorApp.swift
+//  Meme Creator
+//
+//  Created by Darian Mitchell  on 2024/4/21.
+//
+
+import SwiftUI
+
+@main
+struct Meme_CreatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
